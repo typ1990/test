071adf80422aded8071bd323f82f569823e51007
typ1990 <845632422@qq.com>
2017-09-07 20:14:08 +0800

    update sql

--- src/main/sql/schema.sql
+++ src/main/sql/schema.sql
@@ -5,17 +5,17 @@ CREATE DATABASE seckill;
 use seckill;
 --创建表 秒杀库存表
 CREATE  TABLE seckill(
-`seckill_id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT `商品库存id`,
-`name` varchar(120) NOT NULL COMMENT `商品名称`,
-`number` int NOT NULL COMMENT `库存数量`,
-`start_time` TIMESTAMP  not null comment `秒杀开始时间`,
-`end_time` TIMESTAMP  not null comment `秒杀结束时间`,
-`create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP  comment `创建时间`,
+`seckill_id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '商品库存id',
+`name` varchar(120) NOT NULL COMMENT '商品名称',
+`number` int NOT NULL COMMENT '库存数量',
+`start_time` TIMESTAMP  not null comment '秒杀开始时间',
+`end_time` TIMESTAMP  not null comment '秒杀结束时间',
+`create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP  comment '创建时间',
 PRIMARY KEY (seckill_id),
 key idx_start_time(start_time),
 key idx_end_time(end_time),
 key idx_create_time(create_time),
-)ENGINE=InnoDB AUTO_INCREMENT=1000 DEFAULT  CHARSET=utf8 COMMENT=`秒杀库存表`;
+)ENGINE=InnoDB AUTO_INCREMENT=1000 DEFAULT  CHARSET=utf8 COMMENT='秒杀库存表';
 
 CREATE TABLE seckill (
 `seckill_id`  bigint NOT NULL AUTO_INCREMENT ,
